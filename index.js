341d1c89a0f6db95787ba59bd4db99605d217016
IlyaIvanchikov <phantomonster1@mail.ru>
2019-10-10 17:30:53 +0300

    feat: add session

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const routerAdd = require('./routes/add');
 const routerLogin = require('./routes/auth');
 const routerSensors = require('./routes/sensors');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
+const constMiddleware = require('./middleware/variables');
+
+const MONGODB_URI = 'mongodb+srv://IlyaIvanchikov:456455741852www))@weather-km4rd.mongodb.net/detector';
 const app = express();
+
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URI
+});
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -19,6 +29,14 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: 'some secret value',
+    resave: false,
+    saveUninitialized: false,
+    store
+}));
+app.use(constMiddleware);
+
 app.use(routerHome);
 app.use('/add', routerAdd);
 app.use(routerSensors);
@@ -29,8 +47,7 @@ const PORT = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-    const url = 'mongodb+srv://IlyaIvanchikov:456455741852www))@weather-km4rd.mongodb.net/detector';
-    await mongoose.connect(url, {useNewUrlParser: true, 
+    await mongoose.connect(MONGODB_URI, {useNewUrlParser: true, 
                                  useUnifiedTopology: true,
                                  useFindAndModify: false})
     app.listen(PORT, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,25 @@ const {Router} = require("express");
 // const Detectors = require('../models/detector');
 const router = Router();
 
-router.get('/login', async (req, res) => {
+router.get('/auth/login', async (req, res) => {
     res.render('auth/login',
     {
         title: "Авторизация",
         isLogin: true
     });
 });
+
+router.get('/auth/logout', async (req, res) => {
+    req.session.destroy(() => {
+     res.redirect('/auth/login#login');
+    })
+});
+
+router.post('/auth/login', async (req, res) => {
+    req.session.isAuthenticated = true;
+    res.redirect('/');
+});
+
+
 
 module.exports = router;
